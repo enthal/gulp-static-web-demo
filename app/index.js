f2543ac9b1ce302ed6c83fd024bc7bf122ba8c07
Tim James <tjames@tjames.com>
2018-12-11 23:12:53 -0800

    require(...)

--- app/index.js
+++ app/index.js
@@ -1,1 +1,2 @@
-document.querySelector('body').innerText = "Hello JavaScript World!";
+const url = require('url')
+document.body.innerHTML = "URL: "+JSON.stringify(url.parse(window.location.href),0,2)
