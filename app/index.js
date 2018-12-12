dbbc5d8f171c4513d3c3b11ec5cbf85c05ad2c2e
Tim James <tjames@tjames.com>
2018-12-11 23:36:55 -0800

    require from node_modules

--- app/index.js
+++ app/index.js
@@ -1,2 +1,9 @@
 const url = require('url')
-document.body.innerHTML = "URL: "+JSON.stringify(url.parse(window.location.href),0,2)
+const unique = require('uniq')
+const data = [1, 2, 2, 3, 4, 5, 5, 5, 6]
+
+console.log(unique(data));
+document.body.innerHTML =
+  "URL: "+JSON.stringify(url.parse(window.location.href),0,2)
+  +"\n\n"
+  +unique(data);
