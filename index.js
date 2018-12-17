c490bec4cb7457084c46f08fbb51a92c1505867d
Tim James <tjames@tjames.com>
2018-12-16 20:06:38 -0800

    start: gulp dev, FTW!

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const data = [1, 2, 2, 3, 4, 5, 5, 5, 6]
 console.log(unique(data));
 document.body.innerHTML =
   "URL: "+JSON.stringify(url.parse(window.location.href),0,2)
-  +"\n\n"
+  +"\n\nUnique: "
   +unique(data);
