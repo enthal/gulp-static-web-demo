62cb7fe703752089b1906c7c3ee505833e6e70ae
Tim James <tjames@tjames.com>
2018-12-16 20:48:46 -0800

    require relative paths (in ./app)

---
+++ app/thingy.js
@@ -0,0 +1,1 @@
+module.exports = x => "Thingy: "+x
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const url = require('url')
 const unique = require('uniq')
+const app = require('./app')
+const thingy = require('./app/thingy')
+
 const data = [1, 2, 2, 3, 4, 5, 5, 5, 6]
 
 console.log(unique(data));
 document.body.innerHTML =
   "URL: "+JSON.stringify(url.parse(window.location.href),0,2)
   +"\n\nUnique: "
-  +unique(data);
+  +unique(data)
+  +"\n\nRequire relative: "+app(123)
+  +"\n\nRequire relative more: "+thingy('zap')
